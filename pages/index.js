62c13a557138f0f3c4ea403d3aa926a41e649e83
Danilo Borges <daniloborges.dev@gmail.com>
2021-07-19 01:08:48 -0300

    Enter

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 
 import React from 'react';
+import nookies from 'nookies';
+import jwt from 'jsonwebtoken';
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
 import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
@@ -26,9 +28,9 @@ function ProfileSidebar(propriedades) {
 }
 
 
-export default function Home() {
+export default function Home(props) {
 
-    const githubUser = "odaniloborges"
+    const githubUser = props.githubUser;
 
     const [comunidades, setComunidades] = React.useState([{
       //id: '5835137794315195619',
@@ -225,4 +227,40 @@ export default function Home() {
       </MainGrid>
     </>
   )
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context);
+  const token = cookies.USER_TOKEN;
+    
+  const { isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
+    headers: {
+      Authorization: token
+    }
+  })
+  .then((resposta) => resposta.json())
+
+
+  if (!isAuthenticated) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    }
+  }
+
+  // const followers = await fetch(`https://api.github.com/users/${githubUser}/followers`)
+  //   .then((res) => res.json())
+  //   .then(followers => followers.map((follower) => ({
+  //     id: follower.id,
+  //     name: follower.login,
+  //     image: follower.avatar_url,
+  //   })));
+  const { githubUser } = jwt.decode(token);
+  return {
+    props: {
+      githubUser
+    },
+  }
 }
